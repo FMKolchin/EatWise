daff23bb9b4c7802b0e408c90ded90546e7adbb1
OrtalZror <ortal8932@gmail.com>
2023-07-24 15:50:08 +0300

    "add deisign"

--- Client/src/Components/RegisterPersonalDetailsPage/RegisterPersonalDetailsPage.tsx
+++ Client/src/Components/RegisterPersonalDetailsPage/RegisterPersonalDetailsPage.tsx
@@ -1,12 +1,15 @@
 import { Card } from "@mui/material"
 import RegisterPersonalDetails from "../RegisterPersonalDetails/RegisterPersonalDetails"
-
+import '../../App.css'
 
 export const RegisterPersonalDetailsPage = ()=>{
 
     return(
         <Card variant="outlined">
+            <h3 >personal details</h3>
+            <br></br>
             <RegisterPersonalDetails/>
+            <br></br>
             </Card>
     )
 }
--- Client/src/Redux/userSlice/saga/addToDailyWater.ts
+++ Client/src/Redux/userSlice/saga/addToDailyWater.ts
@@ -1,22 +1,22 @@
-import { call, put, takeEvery } from "redux-saga/effects";
-import { actions } from "../slice";
-import { User } from "../../../Models/User";
-import { addWater } from "../../../API/user";
+// import { call, put, takeEvery } from "redux-saga/effects";
+// import { actions } from "../slice";
+// import { User } from "../../../Models/User";
+// import { addWater } from "../../../API/user";
 
-function* onAddToDailyWater(action:any) {
-    try{
-        alert("in dispatch")
-        let user:User = action.payload.user;
-        let water:number = action.payload.water;
-        alert(water);
-        const data:User=yield call(addWater,user.id,water);
-        yield put(actions.onAddToDailyWaterSuccess(data));
-    }
-    catch(e){
-        console.log(e);
-    }
-}
+// function* onAddToDailyWater(action:any) {
+//     try{
+//         alert("in dispatch")
+//         let user:User = action.payload.user;
+//         let water:number = action.payload.water;
+//         alert(water);
+//         const data:User=yield call(addWater,user.id,water);
+//         yield put(actions.onAddToDailyWaterSuccess(data));
+//     }
+//     catch(e){
+//         console.log(e);
+//     }
+// }
 
-export default function* WatchOnAddToDailyWater(){
-    yield takeEvery(actions.onAddToDailyWaterRequest,(action)=>onAddToDailyWater(action));
-}
+// export default function* WatchOnAddToDailyWater(){
+//     yield takeEvery(actions.onAddToDailyWaterRequest,(action)=>onAddToDailyWater(action));
+// }
